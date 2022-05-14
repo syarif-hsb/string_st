2befcc873578519c42869cb8b9fbd62768a12aca
Syarif Hidayatullah <hidayatullah.hsb@gmail.com>
2022-05-14 20:29:10 +0200

    Fix software name on license notices

--- string_st.h
+++ string_st.h
@@ -1,14 +1,14 @@
-/* This file is part of csvr.
+/* This file is part of STRING ST.
  *
- * csvr is free software: you can redistribute it and/or modify it under the terms of the
+ * STRING ST is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version
  * 2 of the License, or (at your option) any later version.
  *
- * csvr is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * STRING ST is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with csvr.
+ * You should have received a copy of the GNU General Public License along with STRING ST.
  * If not, see <https://www.gnu.org/licenses/>.
  */
 
